c9e1cb512729c91b30d9e756a52f0594d0fa3fc9
David Aguilar <djaguilar@gmail.com>
2023-10-17 11:51:52 -0500

    new lightmap lvl1 9

--- catGame/Assets/Scripts/PlayerMovement.cs
+++ catGame/Assets/Scripts/PlayerMovement.cs
@@ -3,96 +3,45 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 
-public class PlayerMovement : MonoBehaviour {
-
-    //Game managing
+public class PlayerMovement : MonoBehaviour
+{
     public GameObject gameUI;
     public GameObject GoalObj;
     public GameObject[] Enemies;
-    
-    //Screen Joystick.
-    private Vector2 scrnCntr; //This will contain the center of screen.
-    private Vector2 center;
-    private Vector2 scrnJoystick; //This will contain the mouse screen position.
-    private float deadStick = .4f;
-	
-	//Character Controller.
-	public float moveSpeed = 8F; //Controls the speed our character runs.
-    public float gravity = 20.0F; //Gravity multiplier.
-    private Vector3 moveDirection = Vector3.zero; //Character's vector of movement.
 
-    //Character Smoothers.
-    //desceleration
+    private CharacterController controller;
+
+    public float moveSpeed = 8F;
+    public float gravity = 20.0F;
     public float rateOfDeceleration = 6f;
-	public float rateOfAcceleration = 6f;
-		//rotation
-	public float turnSpeed = 12f;
-    public float reverse; //this turns the controller into reverse controller
+    public float rateOfAcceleration = 6f;
+    public float turnSpeed = 12f;
+    public float reverse = 1; // Default to not reverse
     public Toggle movToggle;
     public Toggle scrnToggle;
     public bool screenCenter;
 
     private Vector2 currentTouchPosition;
     private Vector2 initialTouchPosition;
-    //Game Functions
-    //**********************************************
+    private Vector2 scrnCntr;
+    private Vector2 center;
+    private Vector2 scrnJoystick;
+    private float deadStick = 0.4f;
 
-
-    void Awake ()
+    void Awake()
     {
-        gameUI = GameObject.FindGameObjectWithTag("UI");
-        GoalObj = GameObject.FindGameObjectWithTag("exit");
-        Enemies = GameObject.FindGameObjectsWithTag("enemy");
-        movToggle = GameObject.Find("ReverseToggle").GetComponent<Toggle>(); //fix this line of code!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        scrnToggle = GameObject.Find("ScreenToggle").GetComponent<Toggle>();
-        scrnCntr.Set(Screen.width * .5f, Screen.height * .5f);       //this halves the resolution to find the center
-
-        if (PlayerPrefs.GetInt("Rev") == 1) //this sets the reverse option to whatever it was set last.
-        {
-            movToggle.isOn = true;
-            reverse = -1;
-        }
-        else if (PlayerPrefs.GetInt("Rev") == 0)
-        {
-            movToggle.isOn = false;
-            reverse = 1;
-        }
-        if (PlayerPrefs.GetInt("Scrn")== 1)
-        {
-            scrnToggle.isOn = true;
-            screenCenter = true;
-        }
-        else if (PlayerPrefs.GetInt("Scrn")==0)
-        {
-            scrnToggle.isOn = false;
-            screenCenter=false;
-        }
+        InitializeReferences();
+        LoadPlayerPrefs();
     }
 
-   /* public void ReverseOnClick()
-    {
-        if (movToggle.isOn == false)
-        {
-            PlayerPrefs.SetInt("Rev", 0);
-            PlayerPrefs.Save();
-            reverse = 1;
-        }
-        else if (movToggle.isOn)
-        {
-            PlayerPrefs.SetInt("Rev", 1);
-            PlayerPrefs.Save();
-            reverse = -1;
-        }
-    }*/
-
-        void Update ()//adds a condition so when detection.seeyou is true, the character movement is set to zero and the caught animation is played.
+    void Update()
     {
         if (GoalObj.GetComponent<ExitLevel>().goalReached)
         {
             PlayerStops();
-
         }
-        else {
+        else
+        {
             if (gameUI.GetComponent<GameOverManager>().seen)
             {
                 FoundByEnemy();
@@ -106,15 +55,33 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    //***********************************************
-    //my functions
+    void InitializeReferences()
+    {
+        controller = GetComponent<CharacterController>();
+        gameUI = GameObject.FindGameObjectWithTag("UI");
+        GoalObj = GameObject.FindGameObjectWithTag("exit");
+        Enemies = GameObject.FindGameObjectsWithTag("enemy");
+        movToggle = GameObject.Find("ReverseToggle").GetComponent<Toggle>();
+        scrnToggle = GameObject.Find("ScreenToggle").GetComponent<Toggle>();
+        scrnCntr = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+    }
+
+    void LoadPlayerPrefs()
+    {
+        int revValue = PlayerPrefs.GetInt("Rev", 0);
+        movToggle.isOn = revValue == 1;
+        reverse = movToggle.isOn ? -1 : 1;
+
+        int scrnValue = PlayerPrefs.GetInt("Scrn", 0);
+        scrnToggle.isOn = scrnValue == 1;
+        screenCenter = scrnToggle.isOn;
+    }
 
     void FoundByEnemy()
     {
-        CharacterController controller = GetComponent<CharacterController>();
         foreach (GameObject currentEnemy in Enemies)
         {
-            if (currentEnemy.GetComponent<Detection>().seeyou) //Just to avoid confusion in the future, this script goes through all the Detection scripts running in the scene...
+            if (currentEnemy.GetComponent<Detection>().seeyou)
             {
                 Vector3 lookAtEnemy = currentEnemy.transform.position - transform.position;
                 lookAtEnemy.y = 0;
@@ -123,48 +90,47 @@ public class PlayerMovement : MonoBehaviour {
 
             if (Vector3.Distance(transform.position, currentEnemy.transform.position) < 1.5f)
             {
-                Vector3 enemyPlayerVec = (transform.position - currentEnemy.transform.position)*2f;
+                Vector3 enemyPlayerVec = (transform.position - currentEnemy.transform.position) * 2f;
                 moveDirection = Vector3.Lerp(moveDirection, new Vector3(enemyPlayerVec.x, 0, enemyPlayerVec.z), rateOfAcceleration * Time.deltaTime);
                 moveDirection.y = 0f;
-                controller.Move(moveDirection * Time.smoothDeltaTime); //applies the motion vector onto the character.
+                controller.Move(moveDirection * Time.smoothDeltaTime);
             }
         }
     }
 
+    private Vector3 moveDirection = Vector3.zero;
 
     void PlayerMove()
     {
-        CharacterController controller = GetComponent<CharacterController>(); //this sets a variable to call the character controller much easier I think.
-        if (controller.isGrounded) //this checks to see if the character is touching the ground.
+        if (controller.isGrounded)
         {
-            if (Input.GetKey(KeyCode.Mouse0)) //this makes sure I'm pressing the mouse button before doing anything.
+            if (Input.GetMouseButton(0))
             {
-                moveDirection = Vector3.Lerp(moveDirection, new Vector3(scrnJoystick.x, 0, scrnJoystick.y), rateOfAcceleration * Time.deltaTime); //for some reason, it feeds a -y number which throws everything off...
+                moveDirection = Vector3.Lerp(moveDirection, new Vector3(scrnJoystick.x, 0, scrnJoystick.y), rateOfAcceleration * Time.deltaTime);
                 moveDirection.y = 0f;
 
-                if (Vector2.SqrMagnitude(scrnJoystick) > deadStick)
+                if (scrnJoystick.sqrMagnitude > deadStick * deadStick)
                 {
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), turnSpeed * Time.deltaTime);//this handles the heading of the character so he always faces the direction he runs.
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), turnSpeed * Time.deltaTime);
                 }
             }
             else
             {
-                moveDirection = Vector3.Lerp(moveDirection, Vector3.zero, rateOfDeceleration * Time.deltaTime); //dampens the deceleration.
+                moveDirection = Vector3.Lerp(moveDirection, Vector3.zero, rateOfDeceleration * Time.deltaTime);
             }
         }
-        moveDirection.y -= gravity * Time.smoothDeltaTime; //applies gravity.
-        controller.Move(moveDirection * Time.smoothDeltaTime); //applies the motion vector onto the character so character keeps falling forward.
+        moveDirection.y -= gravity * Time.smoothDeltaTime;
+        controller.Move(moveDirection * Time.smoothDeltaTime);
     }
 
     void ScreenJoystick()
     {
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0); // Get the first touch
+            Touch touch = Input.GetTouch(0);
 
-            if (screenCenter == true)
+            if (screenCenter)
             {
-                print("screen center is on");
                 center = scrnCntr;
             }
             else
@@ -172,37 +138,27 @@ public class PlayerMovement : MonoBehaviour {
                 if (touch.phase == TouchPhase.Began)
                 {
                     initialTouchPosition = touch.position;
-                    // Now you have the initial screen coordinates of the touch
-                    //Debug.Log("Initial Touch Position: " + initialTouchPosition);
                 }
                 center = initialTouchPosition;
-                print("standard center is on");
             }
 
             currentTouchPosition = touch.position;
-            // Now you have the current screen coordinates of the touch
-            //Debug.Log("Current Touch Position: " + currentTouchPosition);
         }
 
-
-        //this gets the screen position of the mouse on the screen
-        //print(Center);
         scrnJoystick = currentTouchPosition;
-        //this centers it and Normalizes the value(0-10). ????????????????
         scrnJoystick = (scrnJoystick - center);
         scrnJoystick = scrnJoystick * reverse;
         scrnJoystick.Set(scrnJoystick.x / scrnCntr.x * moveSpeed, scrnJoystick.y / scrnCntr.y * moveSpeed);
         scrnJoystick = Vector2.ClampMagnitude(scrnJoystick, moveSpeed);
-        if (Vector2.SqrMagnitude(scrnJoystick) < deadStick)
+        if (scrnJoystick.sqrMagnitude < deadStick * deadStick)
         {
             scrnJoystick = Vector2.zero;
         }
-
     }
+
     void PlayerStops()
     {
-        CharacterController controller = GetComponent<CharacterController>(); //this sets a variable to call the character controller much easier I think.
-        moveDirection = Vector3.Lerp(moveDirection, Vector3.zero, rateOfDeceleration * Time.deltaTime); //dampens the deceleration.
+        moveDirection = Vector3.Lerp(moveDirection, Vector3.zero, rateOfDeceleration * Time.deltaTime);
         controller.Move(moveDirection * Time.smoothDeltaTime);
     }
 }
