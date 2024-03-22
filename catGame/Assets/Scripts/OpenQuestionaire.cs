e935e9017c9e3a0c3271cb3f0f939c93c34558c4
David Aguilar <djaguilar@gmail.com>
2024-03-22 10:07:54 -0500

    added questionaire button

---
+++ catGame/Assets/Scripts/OpenQuestionaire.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenQuestionaire : MonoBehaviour
+{
+    public void OpenQuestions()
+    {
+        Application.OpenURL("https://forms.gle/DQYEvYDru9g3m35E9");
+    }
+}
